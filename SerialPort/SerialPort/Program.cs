fc1198d380975bdfe8cc4d97d0eb58dd87fb610c
Unknown <czarnecki.l.p@gmail.com>
2019-03-20 17:28:05 +0100

    Wyświetlanie dostępnych portów szeregowych

---
+++ SerialPort/SerialPort/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO.Ports;
+
+
+namespace SerialPorts
+{
+    class Program
+    {
+
+            public static void Main()
+            {
+                // Get a list of serial port names.
+                string[] ports = SerialPort.GetPortNames();
+
+                Console.WriteLine("The following serial ports were found:");
+
+                // Display each port name to the console.
+                foreach (string port in ports)
+                {
+                    Console.WriteLine(port);
+                }
+
+                Console.ReadLine();
+            }
+
+    }
+}
