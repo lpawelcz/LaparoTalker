f7ee5f6ad096135d1e13e3fbb6818f81d4ecdda1
lpawelcz <czarnecki.l.p@gmail.com>
2019-03-20 18:22:57 +0100

    Otwieranie i zamykanie wybranego portu

--- SerialPort/SerialPort/Program.cs
+++ SerialPort/SerialPort/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO.Ports;
+using System.Threading;
 
 
 namespace SerialPorts
@@ -7,21 +8,63 @@ namespace SerialPorts
     class Program
     {
 
-            public static void Main()
+        static SerialPort Port;
+        static string portName;
+        public static void Main()
+        {
+            Port = new SerialPort();
+
+            PrintPortNames();
+            Init();
+
+            OpenPort();
+            //          WĄTKI
+            Console.WriteLine("Coś się dzieje");
+            Thread.Sleep(1000);
+            ClosePort();
+            Console.ReadLine();
+        }
+
+        public static void Init()
+        {
+            Console.WriteLine("Podaj nazwe wybranego portu, (Domyslnie: {0}):", "COM1");
+
+            portName = Console.ReadLine();
+            if (portName == "" || !(portName.ToLower()).StartsWith("com"))
+                portName = "COM1";           
+
+            Port.PortName = portName;
+            Port.BaudRate = 9600;
+            Port.Parity = Parity.None;
+            Port.StopBits = StopBits.One;
+            Port.DataBits = 8;
+            Port.Handshake = Handshake.None;
+            Port.ReadTimeout = 500;
+            Port.WriteTimeout = 500;
+        }
+
+        public static void PrintPortNames()
+        {
+            string[] ports = SerialPort.GetPortNames();
+
+            Console.WriteLine("Znaleziono następujące porty szeregowe:");
+            int i = 1;
+            foreach (string port in ports)
             {
-                // Get a list of serial port names.
-                string[] ports = SerialPort.GetPortNames();
-
-                Console.WriteLine("The following serial ports were found:");
-
-                // Display each port name to the console.
-                foreach (string port in ports)
-                {
-                    Console.WriteLine(port);
-                }
-
-                Console.ReadLine();
+                Console.WriteLine("{0}. {1}", i, port);
             }
+        }
 
+        public static void OpenPort()
+        {
+            Port.Open();
+            Console.WriteLine("port {0} zostal otwarty", portName);
+        }
+
+        public static void ClosePort()
+        {
+            Port.Close();
+            Console.WriteLine("port {0} zostal zamkniety", portName);
+        }
     }
 }
