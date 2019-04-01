546967f6906e95c2eaf9f16a60db5165c13a6fe4
lpawelcz <czarnecki.l.p@gmail.com>
2019-04-01 15:47:10 +0200

    Join odkomentowany

--- LaparoTalker/LaparoTalker/Program.cs
+++ LaparoTalker/LaparoTalker/Program.cs
@@ -66,7 +66,7 @@ namespace LaparoTalker
             }
 
 
- //           ListenerThread.Join();
+            ListenerThread.Join();
             PingerThread.Join();
             Thread.Sleep(1000);
             ClosePort();
