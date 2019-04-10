28a4b748346c9773f6f00e2fda60d8bc0475cd27
lpawelcz <czarnecki.l.p@gmail.com>
2019-04-10 09:43:00 +0200

    Odbiór poprawiony

--- LaparoTalker/LaparoTalker/Program.cs
+++ LaparoTalker/LaparoTalker/Program.cs
@@ -17,6 +17,7 @@ namespace LaparoTalker
         static string portName;
         static SerialPort Port = new SerialPort();
         static FlagCarrier _continue = new FlagCarrier();
+        static Logger Logger = new Logger();
         static int order = 0;
         static byte[] command = null;
 
@@ -30,15 +31,14 @@ namespace LaparoTalker
             Thread PingerThread = new Thread(new ThreadStart(Pinger.Run));
             PingerThread.Start();
 
-            ResponseListener Listener = new ResponseListener(Port, ref _continue);
-            Thread ListenerThread = new Thread(new ThreadStart(Listener.Run));
-            ListenerThread.Start();
-
+ //           ResponseListener Listener = new ResponseListener(Port, ref _continue);
+//            Thread ListenerThread = new Thread(new ThreadStart(Listener.Run));
+ //           ListenerThread.Start();
+            Port.DataReceived += new SerialDataReceivedEventHandler(Port_DataReceived);
 
             Console.WriteLine("1.Wyslij CMP\n" + "2.Wyslij CMS\n" + "3. Zakoncz");
             while (_continue.bContinue)
             {
-//              Console.WriteLine("1.Wyslij CMP\n" + "2.Wyslij CMS\n" + "3. Zakoncz");
                 Thread.Sleep(50);
                 do
                 {
@@ -66,7 +66,7 @@ namespace LaparoTalker
             }
 
 
-            ListenerThread.Join();
+ //           ListenerThread.Join();
             PingerThread.Join();
             Thread.Sleep(1000);
             ClosePort();
@@ -74,7 +74,7 @@ namespace LaparoTalker
 
         public static void Init()
         {
-            Port.PortName = portName;
+            Port.PortName = portName;                       // wyszukiwana automatycznie w funkcji FindPortName()
             Port.BaudRate = 9600;
             Port.Parity = Parity.None;
             Port.StopBits = StopBits.One;
@@ -82,6 +82,7 @@ namespace LaparoTalker
             Port.Handshake = Handshake.None;
             Port.ReadTimeout = 500;
             Port.WriteTimeout = 500;
+            Port.ReceivedBytesThreshold = 50;               // WAŻNE: liczba bajtów w buforze odczytu, która uaktywnia zapis do pliku
         }
 
         public static void FindPortName()
@@ -100,7 +101,7 @@ namespace LaparoTalker
                 Console.WriteLine("Status: {0}", ManObj["Status"].ToString());
                 Console.WriteLine("\n");
 #endif
-                if (ManObj["DeviceID"].ToString().Contains("PID_5740"))
+                if (ManObj["DeviceID"].ToString().Contains("PID_6001"))                     //Laparo: PID_5740      myEchoDevice: PID_6001
                 {
                     string[] substrings = ManObj["Name"].ToString().Split('(');
                     substrings = substrings[1].Split(')');
@@ -124,6 +125,22 @@ namespace LaparoTalker
         {
             Port.Close();
             Console.WriteLine("port {0} zostal zamkniety", portName);
+        }
+
+        public static void Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            int dataLength = Port.BytesToRead;
+            byte[] data = new byte[dataLength];
+            int nbrDataRead = Port.Read(data, 0, dataLength);
+            if (nbrDataRead == 0)                                                               // jeśli nie odczytano danych, nie rób nic
+                return;
+
+            string Response = System.Text.Encoding.UTF8.GetString(data, 0, data.Length);        // konwersja bajtów na string
+            Logger.LogWrite(Response);                                                          // Wysłanie danych do pliku
+#if DEBUGin
+            Console.WriteLine("<{0}", Response);
+#endif
+
         }
 
     }
