7f48d536301f170f60b4d7dd410db7fed4496bac
lpawelcz <czarnecki.l.p@gmail.com>
2019-04-24 17:55:42 +0200

    Rozdwojenie jaźni, odczyt urwanych komend

---
+++ LaparoTalker_z_zapisem_ramek/LaparoTalker/Logger.cs
@@ -0,0 +1,49 @@
+﻿//#define EXTENDED_LOG
+
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace LaparoTalker
+{
+
+    public class Logger
+    {
+        private string m_exePath = string.Empty;
+        public string log_name;
+        public Logger(string logname)
+        {
+            log_name = logname;
+        }
+        public void LogWrite(string logMessage)
+        {
+            m_exePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            try
+            {
+                using (StreamWriter w = File.AppendText(m_exePath + "\\" + log_name + ".txt"))
+                {
+                    Log(logMessage, w);
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        public void Log(string logMessage, TextWriter txtWriter)
+        {
+            try
+            {
+#if EXTENDED_LOG
+                txtWriter.WriteLine("{0} {1}: {2}", DateTime.Now.ToLongTimeString(),DateTime.Now.ToShortDateString(), logMessage);
+#else
+                txtWriter.WriteLine("{0}", logMessage);
+#endif
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+
+}
---
+++ LaparoTalker_z_zapisem_ramek/LaparoTalker/Pinger.cs
@@ -0,0 +1,40 @@
+﻿//#define DEBUGin
+
+using System;
+using System.IO.Ports;
+using System.Threading;
+
+namespace LaparoTalker
+{
+    class Pinger
+    {
+        static byte[] CMP = new byte[8];
+        static SerialPort Port;
+        static FlagCarrier continuing;
+        public Pinger(SerialPort port, ref FlagCarrier cont, byte[] CM)
+        {
+            Port = port;
+            continuing = cont;
+            CMP = CM;
+        }
+
+        public void Run()
+        {
+            while (continuing.bContinue)
+            {
+                try
+                {
+                    Port.Write(CMP,0,CMP.Length);
+                    Thread.Sleep(750);
+#if DEBUGin
+                    Console.WriteLine("<{0}", CMP_s);
+#endif
+                }
+                catch (TimeoutException) { }
+            }
+        }
+
+    }
+
+   
+}
